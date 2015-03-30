12dc9077e5229db718a55c14f5e239f0c5e34685
Maksym Chernenko <Maksym.Chernenko@gmail.com>
2015-03-30 22:15:03 +0300

    add simple investigation project

---
+++ LiveProcessInspector/InvestigationApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i = 1;
+			double d = 1.1;
+			string s = "string s";
+			TimeSpan ts = new TimeSpan();
+
+			Console.ReadLine();
+		}
+	}
+}
